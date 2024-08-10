c3aca311863909d3da971b6e054c95dedd16248a
tanittaya <152164438+tanittaya@users.noreply.github.com>
2024-08-10 15:42:40 +0700

    first commit

---
+++ src/Feature/Home.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import data from '../app/data';
+import Product from './Product';
+import AddForm from './Product/Addform';
+
+let currentProductId = 9;
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  function addProduct(product) {
+    const newProduct = { id: ++currentProductId, ...product };
+    setProducts([...products, newProduct]);
+  }
+
+  return (
+    <>
+      <h1>New Products</h1>
+      {
+  products.length > 0 ? (
+    <ul className="Home__products">
+      {products.map((product) => (
+        <Product key={product.id} item={product} />
+      ))}
+    </ul>
+  ) : (
+    <div>Loading products....</div>
+  )
+}
+      <AddForm addProduct={addProduct} />
+    </>
+    
+  );
+}
